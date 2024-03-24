cae751d723608d29807594de7a20e4a131104075
lesuuda <lesuudaljamani@gmail.com>
2024-03-24 20:53:33 +0300

    217-contains_duplicate

---
+++ arrays_and_hashing/217-contains_duplicate.py
@@ -0,0 +1,37 @@
+from typing import List
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+"""
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for numbers in nums:
+            if numbers in hashset:
+                return True
+            hash.add(numbers)
+        return False
