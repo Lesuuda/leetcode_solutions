fd0656a40e5349c0656ea9ad986727e1fb338607
lesuuda <lesuudaljamani@gmail.com>
2024-03-31 06:23:09 +0300

    49-group_anagram medium

---
+++ arrays_and_hashing/49-group_anagrams.py
@@ -0,0 +1,40 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+
+logic: we use a hashmap implemented using dictionsaries. defaultdict is used because it does not raise a KeyError
+ -An array counter is initialised that has 26 zeros. this array is later mapped into the 0-26 integer values and converted to a tuple as it acts as the key of the dictionary pair
+"""
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
